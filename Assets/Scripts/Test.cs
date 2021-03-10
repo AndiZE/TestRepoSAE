55f4caff52f38d2869da4b140e8d51e075245e28
AndiZE <andreaszechmeister@chello.at>
2021-03-10 16:31:22 +0100

    ads

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //"Blub"
+        float y = 0;
     }
 
     // Update is called once per frame
